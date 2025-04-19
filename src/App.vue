ad46f6e6aff0d3700dbecfe5880aaf921dbef7b2
BennyDB <74614235+BennyDeBock@users.noreply.github.com>
2025-04-19 16:46:44 +0200

    Add functionality to add event to calendar
    * Feature/ics event (#2)
    
    * Add helper method to clean up remaining time calculation
    
    * Add creation of calendar event
    
    * Updates location activities

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <main>
-    <template v-for="activity in activities">
-      <CountdownTimer :name="activity.name" :date="activity.date" :background="activity.background"/>
+    <template v-for="activity in activities" :key="activity">
+      <CountdownTimer :name="activity.name" :date="activity.date" :background="activity.background"
+        :duration="activity.duration" :location="activity.location" />
     </template>
   </main>
 </template>
@@ -12,12 +13,14 @@ import type { Activity } from '@/models/Activity';
 import activitiesData from "@/assets/activities.json";
 
 const activities = activitiesData.data.map((activity) => {
-  return <Activity> {
+  return <Activity>{
     name: activity.name,
     date: new Date(activity.date),
-    background: activity.background
+    background: activity.background,
+    duration: activity.duration,
+    location: activity.location,
   }
 }).sort((a, b) => {
   return a.date.getTime() - b.date.getTime()
-  })
+})
 </script>
--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -1,5 +1,17 @@
-export interface Activity{
-  name: string,
-  date: Date,
+export interface Activity {
+  name: string
+  date: Date
+  duration: {
+    hours: number
+    minutes: number
+  }
+  location: string
   background: string
+}
+
+export interface DateVariables {
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
 }
